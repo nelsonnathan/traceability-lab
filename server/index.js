ffb367bb75a198e7c026477f5044aad311086d90
Nate Nelson <nathan.nelson99@gmail.com>
2021-08-17 12:13:18 -0600

    adds rollbar

--- server/index.js
+++ server/index.js
@@ -2,11 +2,19 @@ const express = require('express')
 const path = require('path')
 const port = process.env.PORT || 4545
 
+const Rollbar = require("rollbar");
+const rollbar = new Rollbar({
+  accessToken: 'ad24f23a14f145bf905d2defe4ca66b6',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
 const app = express()
 app.use(express())
 
 
 app.get('/', (req, res) => {
+    rollbar.log("Hello world!");
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
