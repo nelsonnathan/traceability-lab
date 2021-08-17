39788596d7128d03d973677a7dfd527d019e8c21
Nate Nelson <nathan.nelson99@gmail.com>
2021-08-17 12:02:50 -0600

    proper syntax

--- server/index.js
+++ server/index.js
@@ -4,8 +4,10 @@ const port = process.env.PORT || 4545
 
 const app = express()
 
+
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
 app.listen(port, () => console.log(`Oh no! Server ${port} is getting away!`))
